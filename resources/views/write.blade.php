6edb80a76214bb568858c7d3bfe3f1a4c831505b
Arslan Zulfiqar <arslanzulfiqar01@gmail.com>
2022-06-03 22:31:11 +0500

    Modified Design

--- resources/views/write.blade.php
+++ resources/views/write.blade.php
@@ -13,27 +13,23 @@
             </div>
             <div class="col-lg-6 col-md-6 mt-50 padding-60">
                 <p><span style="color: #F7898E;">Structured Edit</span> | Free Edit</p>
-                <div class="table-responsive">
-                    <table class="table table-borderless text-nowrap">
-                        @foreach($result['hyphenated'] as $index=>$hyphenated)
-                            @php $hyphenated_words = array_chunk(explode(' ', $hyphenated), '3'); @endphp
-                            <tbody class="hyphenated-sentence" id="structured_{{ $index }}" @if($index != 0) hidden @endif>
-                            @foreach($hyphenated_words as $hyphenated_word)
-                                <tr>
-                                    @foreach($hyphenated_word as $key=>$value)
-                                        <td>
-                                            <span class="{{ $color[explode('_', $value)[1]]??'line-empty' }}">
-                                                <span>{{ $detail[explode('_', $value)[1]]??'' }}</span>
-                                                {{ explode('_', $value)[0] }}
-                                            </span>
-                                        </td>
-                                    @endforeach
-                                </tr>
-                            @endforeach
-                            </tbody>
+                @foreach($result['hyphenated'] as $index=>$hyphenated)
+                    @php $hyphenated_words = array_chunk(explode(' ', $hyphenated), '3'); @endphp
+                    <div class="hyphenated-sentence" id="structured_{{ $index }}" @if($index != 0) hidden @endif>
+                        @foreach($hyphenated_words as $hyphenated_word)
+                            <ul style="display: flex;" class="line-height-40">
+                                @foreach($hyphenated_word as $key=>$value)
+                                    <li> <span class="{{ $color[explode('_', $value)[1]]??'line-empty' }}">{{ explode('_', $value)[0] }}</span> <p class="line-yellow-1">{{ $detail[explode('_', $value)[1]]??'' }}</p></li>
+                                @endforeach
+                            </ul>
                         @endforeach
-                    </table>
-                </div>
+                    </div>
+                @endforeach
+                <ul class="pagination" style="margin-top: 30px;">
+                    <li class="page-item"><a class="page-link" href="#">&laquo; Previous Sentence</a></li>
+                    <li class="page-item"><a class="page-link" href="#">View all</a></li>
+                    <li class="page-item"><a class="page-link" href="#">Next Sentence &raquo; </a></li>
+                </ul>
             </div>
         </div>
 
